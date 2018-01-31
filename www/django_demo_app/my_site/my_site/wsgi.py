26be8df11ac0618e9b77b7cbb7c1dc4055d62b41
StephenGodderidge <steve.godderidge@gmail.com>
2018-01-31 16:18:31 -0700

    personalized project to have basic django 2 app

--- www/django_demo_app/my_site/my_site/wsgi.py
+++ www/django_demo_app/my_site/my_site/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for demo_site project.
+WSGI config for my_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_site.settings")
 
 application = get_wsgi_application()
